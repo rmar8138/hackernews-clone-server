8dfdfdaf661e4e25f605ccc3eef7dfae80ac4d26
Ragan Martinez <ragan.martinez@live.com>
2020-02-20 14:21:38 +1100

    User auth login and signup with jwt and pw hashing

--- src/index.js
+++ src/index.js
@@ -1,35 +1,21 @@
 const { GraphQLServer } = require("graphql-yoga");
 const { prisma } = require("./generated/prisma-client");
-
-let links = [
-  {
-    id: "link-0",
-    url: "www.howtographql.com",
-    description: "Fullstack tutorial for GraphQL"
-  }
-];
-let idCount = links.length;
+const Query = require("./resolvers/Query");
+const Mutation = require("./resolvers/Mutation");
+const User = require("./resolvers/User");
+const Link = require("./resolvers/Link");
 
 const resolvers = {
-  Query: {
-    info: () => "This is the API of a Hackernews clone",
-    feed: (root, args, context, info) => context.prisma.links()
-  },
-  Mutation: {
-    post: (root, args, context) => {
-      const { url, description } = args;
-      return context.prisma.createLink({
-        url,
-        description
-      });
-    }
-  }
+  Query,
+  Mutation,
+  User,
+  Link
 };
 
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
   resolvers,
-  context: { prisma }
+  context: request => ({ ...request, prisma })
 });
 
 server.start(() => console.log("Serving up on port 4000!"));
--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,7 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { APP_SECRET, getUserId } = require("./../utils");
+
 const signup = async (parent, args, context, info) => {
   // create password hash
   const password = await bcrypt.hash(args.password, 10);
@@ -16,8 +20,7 @@ const signup = async (parent, args, context, info) => {
 
 const login = async (parent, args, context, info) => {
   // find user
-  const user = context.prisma.user({ email: args.email });
-
+  const user = await context.prisma.user({ email: args.email });
   if (!user) {
     throw new Error("No such user found");
   }
@@ -38,7 +41,18 @@ const login = async (parent, args, context, info) => {
   };
 };
 
+const post = async (parent, args, context, info) => {
+  // get userId from token
+  const userId = getUserId(context);
+  return context.prisma.createLink({
+    url: args.url,
+    description: args.description,
+    postedBy: { connect: { id: userId } }
+  });
+};
+
 module.exports = {
   signup,
-  login
+  login,
+  post
 };
